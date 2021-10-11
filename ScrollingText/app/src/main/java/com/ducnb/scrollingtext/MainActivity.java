9692e8fe444c222719b3b81378cacf5a93c65ba6
Duc <62656512+lukamix@users.noreply.github.com>
2021-10-11 16:03:00 +0700

    Add CodeLab 01.3 Code

---
+++ ScrollingText/app/src/main/java/com/ducnb/scrollingtext/MainActivity.java
@@ -0,0 +1,14 @@
+package com.ducnb.scrollingtext;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
