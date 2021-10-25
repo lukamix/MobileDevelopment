2b54362c0ef87fef8ec35637349d1becfc315e9d
Duc <62656512+lukamix@users.noreply.github.com>
2021-10-25 21:04:30 +0700

    Add Codelab 03.3

---
+++ Codelab 03.3/HelloCompat/app/src/main/java/com/ducnb/hellocompat/MainActivity.java
@@ -0,0 +1,49 @@
+package com.ducnb.hellocompat;
+
+import androidx.annotation.RequiresApi;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
+
+import android.os.Build;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import java.util.Random;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TextView mHelloTextView;
+    private String[] mColorArray = {"red", "pink", "purple", "deep_purple",
+            "indigo", "blue", "light_blue", "cyan", "teal", "green",
+            "light_green", "lime", "yellow", "amber", "orange", "deep_orange",
+            "brown", "grey", "blue_grey", "black" };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mHelloTextView = findViewById(R.id.hello_textview);
+        // restore saved instance state (the text color)
+        if (savedInstanceState != null) {
+            mHelloTextView.setTextColor(savedInstanceState.getInt("color"));
+        }
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        // save the current text color
+        outState.putInt("color", mHelloTextView.getCurrentTextColor());
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.M)
+    public void changeColor(View view) {
+        Random random = new Random();
+        String colorName = mColorArray[random.nextInt(20)];
+        int colorResourceName = getResources().getIdentifier(colorName,
+                "color", getApplicationContext().getPackageName());
+        int colorRes = ContextCompat.getColor(this, colorResourceName);
+        mHelloTextView.setTextColor(colorRes);
+    }
+}
