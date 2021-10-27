182a9e3dff5a3146c537b4b1df02830ba7614a1d
Duc <62656512+lukamix@users.noreply.github.com>
2021-10-27 20:03:00 +0700

    Add UI

---
+++ Calculator/app/src/main/java/com/ducnb/calculator/MainActivity.java
@@ -0,0 +1,105 @@
+package com.ducnb.calculator;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void onClickMCButton(View view) {
+    }
+
+    public void onClickMRButton(View view) {
+    }
+
+    public void onClickMPlusButton(View view) {
+    }
+
+    public void onClickMSubtractButton(View view) {
+    }
+
+    public void onClickMSButton(View view) {
+    }
+
+    public void onClickMButton(View view) {
+    }
+
+    public void onClickPercentButton(View view) {
+    }
+
+    public void onClickCEButton(View view) {
+    }
+
+    public void onClickCButton(View view) {
+    }
+
+    public void onClickBackButton(View view) {
+    }
+
+    public void onClickReverseButton(View view) {
+    }
+
+    public void onClickSquareButton(View view) {
+    }
+
+    public void onClickSquareRootButton(View view) {
+    }
+
+    public void onClickDevideButton(View view) {
+    }
+
+    public void onClickSevenButton(View view) {
+    }
+
+    public void onClickEightButton(View view) {
+    }
+
+    public void onClickNineButton(View view) {
+    }
+
+    public void onClickMultipleButton(View view) {
+    }
+
+    public void onClickFourButton(View view) {
+    }
+
+    public void onClickFiveButton(View view) {
+    }
+
+    public void onClickSixButton(View view) {
+    }
+
+    public void onClickSubtractButton(View view) {
+    }
+
+    public void onClickOneButton(View view) {
+    }
+
+    public void onClickTwoButton(View view) {
+    }
+
+    public void onClickThreeButton(View view) {
+    }
+
+    public void onClickPlusButton(View view) {
+    }
+
+    public void onClickNegativeButton(View view) {
+    }
+
+    public void onClickZeroButton(View view) {
+    }
+
+    public void onClickDotButton(View view) {
+    }
+
+    public void onClickEqualButton(View view) {
+    }
+}
